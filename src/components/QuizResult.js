5a85bacd39135b27d3aa33a0392ea0e040c3ff60
archi <senarchi4@gmail.com>
2022-11-07 20:43:21 +0530

    Speedometer

--- src/components/QuizResult.js
+++ src/components/QuizResult.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import ReactSpeedometer from 'react-d3-speedometer'
 
 const QuizResult = ({result,retry}) => {
   return (
 
     <div className='result-screen'>
-    <h2>Result: {result.percentage}%</h2>
+    <ReactSpeedometer
+      width={250}
+   height={250}
+   maxValue={100}
+   ringWidth={30}
+  value={result.percentage}
+  needleColor="black"
+  startColor="red"
+  segments={1000}
+  maxSegmentLabels={5}
+  endColor="green"
+  needleTransition="easeElastic"
+    needleHeightRatio={0.5}
+    />
+    <h2 className='result'>Result: {result.percentage}%</h2>
     <p className='correct'>{result.correct} Correct</p>
     <p className='incorrect'> {result.total-result.correct} Incorrect</p> 
     <button onClick={retry}>Retry</button>
